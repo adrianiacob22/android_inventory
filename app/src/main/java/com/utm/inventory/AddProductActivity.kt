19cf95b622ca5cb8a7069672e0b556770a49a1b6
Adrian Iacob <adrian@oc2627300782.ibm.com>
2020-05-26 16:46:59 +0300

    stocheaza produsul in baza

--- app/src/main/java/com/utm/inventory/AddProductActivity.kt
+++ app/src/main/java/com/utm/inventory/AddProductActivity.kt
@@ -1,13 +1,25 @@
 package com.utm.inventory
 
+import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.add_product.*
 
 class AddProductActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.add_product)
+
+        addProductSubmitButton.setOnClickListener {
+
+            val database = getSharedPreferences("database", Context.MODE_PRIVATE)
+            database.edit().apply {
+                putString("savedProductName", editTextProductName.text.toString())
+            }.apply()
+        }
+
+
     }
 
 }
--- app/src/main/java/com/utm/inventory/MainActivity.kt
+++ app/src/main/java/com/utm/inventory/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.utm.inventory
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log.d
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +16,14 @@ class MainActivity : AppCompatActivity() {
         goToAddProduct.setOnClickListener {
             startActivity(Intent(this, AddProductActivity::class.java))
         }
+
+        val preferences = getSharedPreferences( "database", Context.MODE_PRIVATE)
+        val savedName = preferences.getString("savedProductName", "Valoarea nu exista")
+        d( "Adrian", "Mesajul salvat este: $savedName")
+
+        lastSavedProduct.text = savedName
+
+
 
     }
 }
